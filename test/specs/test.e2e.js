1a2c2e97ff4d3f43f271461e7e1a8623fe0a3ff0
iniyavans <iniyavan@sparkouttech.com>
2023-12-03 21:08:06 +0530

    Update test file

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,8 +2,8 @@ const { expect } = require('@wdio/globals')
 const LoginPage = require('../pageobjects/nftMarketplace.page')
 const SecurePage = require('../pageobjects/secure.page')
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+describe('Verifing the NFT Marketplace page has a valid content', () => {
+    it('should have a NFT Marketplace heading', async () => {
         await LoginPage.open()
         await expect(SecurePage.flashAlert).toBeExisting()
     })
