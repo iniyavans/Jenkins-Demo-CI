f5c3063f4741368aed54410de537e5e8da6e6a31
Iniyavan <88368294+iniyavans@users.noreply.github.com>
2023-12-09 11:24:41 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,9 +51,11 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome'/*,
+        'goog:chromeOptions': { 
+            args: ['--headless', '--disable-gpu']
          // Enable headless mode and disable GPU acceleration for Chrome
-        }
+        } */
     }],
 
     //
